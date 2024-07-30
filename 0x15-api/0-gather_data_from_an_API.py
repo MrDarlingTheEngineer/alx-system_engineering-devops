eec66c79eeb92885c0418a072bbdcdbb98e46427
Mr_Darling <117775164+MrDarlingTheEngineer@users.noreply.github.com>
2024-07-30 00:25:38 +0000

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """using the JSONPlaceholder API:
    returns information about the TODO list progress for a given employee ID"""
-
 import requests
-from sys import argv
 
-if __name__ == '__main__':
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(argv[1])).json()
-    todo = requests.get(url + "todos", params={"userId": argv[1]}).json()
-    tasks = []
-    for task in todo:
-        if task.get('completed') is True:
-            tasks.append(task.get('title'))
-    print("Employee {} is done with tasks({}/{}):".
-          format(user.get('name'), len(tasks), len(todo)))
-    for task in tasks:
-        print("\t {}".format(task))
+def get_todo_list_progress(employee_id):
+    url = f"(link unavailable)"
+    response = requests.get(url)
+    data = response.json()
+
+    employee_name = data["employee_name"]
+    total_tasks = len(data["todos"])
+    completed_tasks = [task for task in data["todos"] if task["completed"]]
+    num_done_tasks = len(completed_tasks)
+
+    print(f"Employee {employee_name} is done with tasks({num_done_tasks}/{total_tasks}):")
+
+    for task in completed_tasks:
+        print(f"\t{task['title']}")
+
+if __name__ == "__main__":
+    employee_id = int(input("Enter employee ID: "))
+    get_todo_list_progress(employee_id)
